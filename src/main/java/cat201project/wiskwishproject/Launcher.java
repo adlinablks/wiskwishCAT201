5d1b34a81d4be85a27422a9e60bcbbf4bea5968a
NUR ADLINA BALKIS BINTI MUHAMMAD RAZALI <balkisadlina@gmail.com>
2025-12-11 01:22:03 +0800

    Initial commit

---
+++ src/main/java/cat201project/wiskwishproject/Launcher.java
@@ -0,0 +1,9 @@
+package cat201project.wiskwishproject;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module cat201project.wiskwishproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens cat201project.wiskwishproject to javafx.fxml;
+    exports cat201project.wiskwishproject;
+}
