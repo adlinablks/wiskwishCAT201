0294832820447528f8fee6643c9fe5764f2f3331
auni <auniamirafauzi@gmail.com>
2025-12-20 01:04:47 +0800

    fix path for jservlets

--- src/main/java/cat201project/wiskwishproject/Launcher.java
+++
@@ -1,9 +0,0 @@
-package cat201project.wiskwishproject;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
