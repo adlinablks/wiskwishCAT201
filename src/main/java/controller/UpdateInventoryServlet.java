2a01379afc9445040ca133a54f0bdb8715c5323c
Nur Insyirah <nurinsyirah281005@gmail.com>
2026-01-09 18:32:29 +0800

    update update-inventory

--- src/main/java/controller/UpdateInventoryServlet.java
+++ src/main/java/controller/UpdateInventoryServlet.java
@@ -1,4 +1,4 @@
-package com.wiskwish.controller;
+package controller;
 
 import java.io.*;
 import jakarta.servlet.ServletException;
@@ -13,6 +13,7 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import cat201project.model.InventoryItem;
 
 @WebServlet("/UpdateInventoryServlet")
 public class UpdateInventoryServlet extends HttpServlet {
@@ -149,35 +150,5 @@ public class UpdateInventoryServlet extends HttpServlet {
             e.printStackTrace();
             return false;
         }
-    }
-
-    // Inner class for inventory item
-    public static class InventoryItem {
-        private String cakeId;
-        private String tier;
-        private String flavour;
-        private String size;
-        private int quantity;
-        private long lastUpdated;
-
-        public InventoryItem() {}
-
-        public String getCakeId() { return cakeId; }
-        public void setCakeId(String cakeId) { this.cakeId = cakeId; }
-
-        public String getTier() { return tier; }
-        public void setTier(String tier) { this.tier = tier; }
-
-        public String getFlavour() { return flavour; }
-        public void setFlavour(String flavour) { this.flavour = flavour; }
-
-        public String getSize() { return size; }
-        public void setSize(String size) { this.size = size; }
-
-        public int getQuantity() { return quantity; }
-        public void setQuantity(int quantity) { this.quantity = quantity; }
-
-        public long getLastUpdated() { return lastUpdated; }
-        public void setLastUpdated(long lastUpdated) { this.lastUpdated = lastUpdated; }
     }
 }
